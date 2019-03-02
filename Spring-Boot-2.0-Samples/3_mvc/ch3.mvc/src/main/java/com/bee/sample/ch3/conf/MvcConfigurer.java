bbfd049bef6858aada12f7bbf366b1a6843a0dfd
muhua <microhuang@live.com>
2019-03-02 23:31:46 +0800

    Update MvcConfigurer.java

--- Spring-Boot-2.0-Samples/3_mvc/ch3.mvc/src/main/java/com/bee/sample/ch3/conf/MvcConfigurer.java
+++ Spring-Boot-2.0-Samples/3_mvc/ch3.mvc/src/main/java/com/bee/sample/ch3/conf/MvcConfigurer.java
@@ -53,6 +53,13 @@ public class MvcConfigurer implements WebMvcConfigurer {
 			}
 			return true;
 		}
+		public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+			//TODO:
+		}
+		@Override
+		public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+			//TODO:
+		}
 	}
 	
 	
